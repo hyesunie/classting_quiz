61f9983409fba269fb8b071e41100332d323b83f
hyesunie <hyesunie1025@gmail.com>
2022-07-25 20:20:36 +0900

    feat: home 페이지 라우팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React, { ReactElement } from 'react';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './pages/Home';
 
 function App(): ReactElement {
-  return <div className="App" />;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  );
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,30 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
-import Home from './pages/Home';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />}>
-        <Route index element={<Home />} />
-        {/* <Route path="teams" element={<Teams />}> */}
-        {/* <Route path=":teamId" element={<Team />} /> */}
-        {/* <Route path="new" element={<NewTeamForm />} /> */}
-        {/* <Route index element={<LeagueStandings />} /> */}
-        {/* </Route> */}
-      </Route>
-    </Routes>
+    <App />
   </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
