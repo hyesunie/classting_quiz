923693704c3e883b647c747ddf386b4d134b832d
hyesunie <hyesunie1025@gmail.com>
2022-07-25 23:02:31 +0900

    feat: 퀴즈 시작 버튼 구현

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,15 @@
 import React, { ReactElement } from 'react';
+import './Home.css';
+import { Link } from 'react-router-dom';
 
 function Home(): ReactElement {
-  return <div>home</div>;
+  return (
+    <div className="home-wrapper">
+      <Link to="/">
+        <div className="home__start-button">퀴즈 시작하기!!</div>
+      </Link>
+    </div>
+  );
 }
 
 export default Home;
