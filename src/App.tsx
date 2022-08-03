027a21cfc1653b733f5c941c6bdcc18c6c563c71
hyesunie <hyesunie1025@gmail.com>
2022-08-03 15:03:09 +0900

    feat: quizList api 호출 후 저장 기능 구현

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React, { ReactElement } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
+import Quiz from './pages/Quiz';
 
 function App(): ReactElement {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/quiz/:id" element={<Quiz />} />
     </Routes>
   );
 }
