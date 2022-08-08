ceb6af001dd33a25f08ae138487144f357e3d7e2
hyesunie <hyesunie1025@gmail.com>
2022-08-08 15:54:40 +0900

    feat: Timer 기능 구현

---
+++ src/components/Timer.tsx
@@ -0,0 +1,33 @@
+import React, {
+  createContext,
+  ReactElement,
+  useEffect,
+  useMemo,
+  useState,
+} from 'react';
+
+export interface TimerProps {
+  isStop: boolean;
+}
+
+const Timer: React.FC<TimerProps> = ({ isStop }: TimerProps): ReactElement => {
+  const [currentTime, setCurrentTime] = useState<number>(0);
+  const [id, setId] = useState(() => {
+    let countTime = 0;
+
+    return setInterval(() => {
+      countTime += 1;
+      setCurrentTime(countTime);
+    }, 1000);
+  });
+
+  useEffect(() => {
+    if (isStop) {
+      clearInterval(id);
+    }
+  }, [isStop, id]);
+
+  return <div>{`${Math.floor(currentTime / 60)}분 ${currentTime % 60}초`}</div>;
+};
+
+export default Timer;
