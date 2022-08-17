ccfdd7b1823012f757d8fdd383788cc723c68d67
hyesunie <hyesunie1025@gmail.com>
2022-08-17 17:29:51 +0900

    refactor: 타이머 삭제 더 효율적인 방법으로 수정

--- src/pages/Result.tsx
+++ src/pages/Result.tsx
@@ -1,14 +1,19 @@
-import React, { ReactElement, useState } from 'react';
+import React, { ReactElement, useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
+import { changeTimeFormat } from '../components/Timer';
 import { QuizInfo } from './Home';
 
 const Result: React.FC = (): ReactElement => {
   const { state } = useLocation();
-  const [time, _] = useState(localStorage.getItem('Time'));
+  const [time, setTime] = useState<number>(0);
   const quizList = state as QuizInfo[];
 
   let trueCnt = 0;
   let falseCnt = 0;
+
+  useEffect(() => {
+    setTime(Number(localStorage.getItem('Time')));
+  }, []);
 
   for (let i = 0; i < quizList.length; i += 1) {
     if (quizList[i].isAnswer) {
@@ -23,9 +28,10 @@ const Result: React.FC = (): ReactElement => {
       <section>
         <div>
           문제 수: {quizList.length}, 맞춘 문제: {trueCnt}, 틀린 문제:{falseCnt}
-          , 걸린시간:{time}초
+          , 걸린시간:{changeTimeFormat(Number(time))}
         </div>
       </section>
+      <section />
     </div>
   );
 };
