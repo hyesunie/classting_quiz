133e29d6f97a8896a5221aef0c389fa887d0844f
hyesunie <hyesunie1025@gmail.com>
2022-08-18 14:58:23 +0900

    feat: 페이지 구조 구체화

--- src/pages/Result.tsx
+++ src/pages/Result.tsx
@@ -1,37 +1,50 @@
 import React, { ReactElement, useEffect, useState } from 'react';
-import { useLocation } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { changeTimeFormat } from '../components/Timer';
 import { QuizInfo } from './Home';
+
+interface ResultCount {
+  trueCnt: number;
+  falseCnt: number;
+}
 
 const Result: React.FC = (): ReactElement => {
   const { state } = useLocation();
   const [time, setTime] = useState<number>(0);
+  const [resultObj, setResultObj] = useState<ResultCount>({
+    trueCnt: 0,
+    falseCnt: 0,
+  });
   const quizList = state as QuizInfo[];
-
-  let trueCnt = 0;
-  let falseCnt = 0;
 
   useEffect(() => {
     setTime(Number(localStorage.getItem('Time')));
-  }, []);
 
-  for (let i = 0; i < quizList.length; i += 1) {
-    if (quizList[i].isAnswer) {
-      trueCnt += 1;
-    } else {
-      falseCnt += 1;
+    let tCnt = 0;
+    let fCnt = 0;
+
+    for (let i = 0; i < quizList.length; i += 1) {
+      if (quizList[i].isAnswer) {
+        tCnt += 1;
+      } else {
+        fCnt += 1;
+      }
     }
-  }
+
+    setResultObj({ trueCnt: tCnt, falseCnt: fCnt });
+  }, []);
 
   return (
     <div>
       <section>
         <div>
-          문제 수: {quizList.length}, 맞춘 문제: {trueCnt}, 틀린 문제:{falseCnt}
-          , 걸린시간:{changeTimeFormat(Number(time))}
+          문제 수: {quizList.length}, 맞춘 문제: {resultObj.trueCnt}, 틀린 문제:
+          {resultObj.falseCnt}, 걸린시간:{changeTimeFormat(Number(time))}
         </div>
       </section>
-      <section />
+      <section>
+        <Link to="/">다시 풀기</Link>
+      </section>
     </div>
   );
 };
