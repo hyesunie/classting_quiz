3446f967fa4b3e8342e38eec15b19dc0b6abec0c
hyesunie <hyesunie1025@gmail.com>
2022-08-30 23:06:54 +0900

    chore: BrowserRouter => HashRouter 로 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { ReactElement } from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
 import Quiz from './pages/Quiz';
@@ -8,14 +8,14 @@ import WrongAnswerNote from './pages/WrongAnswerNote';
 
 function App(): ReactElement {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/quiz/:id" element={<Quiz />} />
         <Route path="/result" element={<Result />} />
         <Route path="/wrong-answer-note" element={<WrongAnswerNote />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
