937db3fadd9cdcf664f7dca08ee0472a1e80a641
hyesunie <hyesunie1025@gmail.com>
2022-08-30 23:00:34 +0900

    chore: 베이스 url 지정

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { ReactElement } from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
 import Quiz from './pages/Quiz';
@@ -8,12 +8,14 @@ import WrongAnswerNote from './pages/WrongAnswerNote';
 
 function App(): ReactElement {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/quiz/:id" element={<Quiz />} />
-      <Route path="/result" element={<Result />} />
-      <Route path="/wrong-answer-note" element={<WrongAnswerNote />} />
-    </Routes>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/quiz/:id" element={<Quiz />} />
+        <Route path="/result" element={<Result />} />
+        <Route path="/wrong-answer-note" element={<WrongAnswerNote />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
